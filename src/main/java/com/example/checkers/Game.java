c28185bc44f2001dbe47fe40ab93cdb4727dfef6
Bartek <bartiqpuchala@gmail.com>
2022-12-30 20:15:51 +0100

    New server validation method and changed check_move method

--- src/main/java/com/example/checkers/Game.java
+++ src/main/java/com/example/checkers/Game.java
@@ -18,6 +18,7 @@ public class Game extends CheckersApp implements Runnable {
   private static int turn=FIRST;
   String line;
   private Square[][] squares = new Square[Size][Size];
+
   public Game(Socket firstPlayer, Socket secondPlayer) {
     this.firstPlayer = firstPlayer;
     this.secondPlayer = secondPlayer;
@@ -27,18 +28,6 @@ public class Game extends CheckersApp implements Runnable {
     return false;
   }
 
-  /*
-  public String check_moveCentre(Piece piece) {
-    int newX = (int) ((piece.getCenterX() - piece.getCenterX()%CheckersApp.PieceSize)/CheckersApp.PieceSize);
-    int newY = (int) ((piece.getCenterY() - piece.getCenterY()%CheckersApp.PieceSize)/CheckersApp.PieceSize);
-    if((newX + newY)%2 == 1) {
-      return "valid"; // move is valid
-    }
-    else{
-      return "not_valid"; // move is not valid
-    }
-  }
-*/
   public void createBoard(){
     for(i=0 ; i<Size ; i++) {
       for(j=0 ; j<Size ; j++){
@@ -57,36 +46,53 @@ public class Game extends CheckersApp implements Runnable {
     }
   }
 
-  public boolean check_move(int oldX, int oldY, int newX, int newY){
-    if((newX + newY)%2 == 1){
-      if(squares[newX][newY].getPiece() == null){
+  public boolean check_move(int oldX, int oldY, int newX, int newY) {
+    if((newX + newY) % 2 == 1) {
+      if(squares[newX][newY].getPiece() == null) {
         Piece newPiece = squares[oldX][oldY].getPiece();
+        System.out.println(newPiece);
+        newPiece.setCenterX(newX*CheckersApp.PieceSize+CheckersApp.PieceSize*0.5);
+        newPiece.setCenterY(newY*CheckersApp.PieceSize+CheckersApp.PieceSize*0.5);
+        newPiece.setX_pos(newX);
+        newPiece.setY_pos(newY);
+        newPiece.setOldX(newPiece.getCenterX() - newPiece.getCenterX() % CheckersApp.PieceSize + CheckersApp.PieceSize * 0.5);
+        newPiece.setOldY(newPiece.getCenterY() - newPiece.getCenterY() % CheckersApp.PieceSize + CheckersApp.PieceSize * 0.5);
+//        newPiece.
         squares[oldX][oldY].setPiece(null);
         squares[newX][newY].setPiece(newPiece);
+        System.out.println(squares[newX][newY].getPiece());
         return true;
       }
     }
     return false;
   }
 
+  public boolean checkIfMatted(int oldX, int oldY, int newX, int newY) {
+    if(((oldX + newX) % 2 == 0) && ((oldY + newY) % 2 == 0)) {
+      if(squares[(oldX + newX) / 2][(oldY + newY) / 2].getPiece().getPaint() != squares[newX][newY].getPiece().getPaint()) {
+        // zwrócenie prawdy do klienta i usunięcie pośredniczącego pionka z planszy
+        squares[(oldX + newX) / 2][(oldY + newY) / 2].setPiece(null);
+        return true;
+      }
+    }
+    return false;
+  }
+
+
   @Override
   public void run() {
     createBoard();
       try {
         InputStream input_first = firstPlayer.getInputStream();
-        //ObjectInputStream objInput_first = new ObjectInputStream(input_first);
         BufferedReader in_first = new BufferedReader(new InputStreamReader(input_first));
 
         InputStream input_second = secondPlayer.getInputStream();
-        //ObjectInputStream objInput_second = new ObjectInputStream(input_second);
         BufferedReader in_second = new BufferedReader(new InputStreamReader(input_second));
 
         OutputStream output_first = firstPlayer.getOutputStream();
-        //ObjectOutputStream objOutput_first = new ObjectOutputStream(output_first);
         PrintWriter out_first = new PrintWriter(output_first, true);
 
         OutputStream output_second = secondPlayer.getOutputStream();
-        //ObjectOutputStream objOutput_second = new ObjectOutputStream(output_second);
         PrintWriter out_second = new PrintWriter(output_second, true);
 
         out_first.println("1");
@@ -95,13 +101,26 @@ public class Game extends CheckersApp implements Runnable {
 
         do {
           if (turn == SECOND) { // Ruch czarnych
-            //System.out.println("SECOND");;
             line = in_second.readLine();
             String[] data_piece = line.split(" ");
-            PieceType type;
-            //String response = check_moveCentre(piece);
-            //PrintWriter out2 = new PrintWriter(secondPlayer.getOutputStream(), true);
-            if(check_move(parseInt(data_piece[0]), parseInt(data_piece[1]), parseInt(data_piece[3]), parseInt(data_piece[4]))){
+            int oldX = parseInt(data_piece[0]);
+            int oldY = parseInt(data_piece[1]);
+            int newX = parseInt(data_piece[3]);
+            int newY = parseInt(data_piece[4]);
+            boolean checkMove = check_move(oldX, oldY, newX, newY);
+            boolean checkMat = checkIfMatted(oldX, oldY, newX, newY);
+
+            if(checkMove && checkMat) {
+              out_second.println("valid " + line + " matted");
+              System.out.println("valid " + line + " matted");
+              out_first.println("valid " + line + " matted");
+              System.out.println("move " + line + " matted");
+              // Podwójne zbijanie (dodać itp)
+              turn = FIRST;
+            }
+
+            else if(checkMove && !checkMat){
+//              System.out.println(checkIfMatted(parseInt(data_piece[0]), parseInt(data_piece[1]), parseInt(data_piece[3]), parseInt(data_piece[4])));
               out_second.println("valid " + line);
               System.out.println("valid " + line);
               out_first.println("valid " + line);
@@ -113,31 +132,46 @@ public class Game extends CheckersApp implements Runnable {
               out_first.println("not " + line);
               System.out.println("not " + line);
             }
-            //out_first.println("valid");
-            //out_second.println("valid");
-            //System.out.println("tez juz");
+
           }
           if (turn == FIRST) { // Ruch białych
             //System.out.println("FIRST");
             line = in_first.readLine();
             String[] data_piece = line.split(" ");
-            PieceType type;
+            int oldX = parseInt(data_piece[0]);
+            int oldY = parseInt(data_piece[1]);
+            int newX = parseInt(data_piece[3]);
+            int newY = parseInt(data_piece[4]);
             //PrintWriter out1 = new PrintWriter(firstPlayer.getOutputStream(), true);
-            if(check_move(parseInt(data_piece[0]), parseInt(data_piece[1]), parseInt(data_piece[3]), parseInt(data_piece[4]))){
+//            System.out.println(( check_move(oldX, oldY, newX, newY) && !checkIfMatted(oldX, oldY, newX, newY) ));
+
+            boolean checkMove = check_move(oldX, oldY, newX, newY);
+            boolean checkMat = checkIfMatted(oldX, oldY, newX, newY);
+
+
+            if(checkMove && checkMat) {
+              System.out.println("Jestem tu w 1 ifie");
+              out_first.println("valid " + line + " matted");
+              System.out.println("valid " + line + " matted");
+              out_second.println("valid " + line + " matted");
+              System.out.println("move " + line + " matted");
+              turn = SECOND;
+            }
+            else if(checkMove && !checkMat) {
+//              System.out.println(checkIfMatted(parseInt(data_piece[0]), parseInt(data_piece[1]), parseInt(data_piece[3]), parseInt(data_piece[4])));
               out_first.println("valid " + line);
               System.out.println("valid " + line);
               out_second.println("valid " + line);
               System.out.println("move " + line);
               turn = SECOND;
-            } else{
+            }
+            else{
               out_first.println("not " + line);
               System.out.println("not " + line);
               out_second.println("not " + line);
               System.out.println("not " + line);
             }
             //String response = check_moveCentre(piece);
-            //System.out.println("tez juz");
-            //out_second.println("valid");
           }
 
         } while (!gameEnded);
