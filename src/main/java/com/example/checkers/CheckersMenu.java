a77f19cd38a677d75d681886419f6a03c8afcd8f
Bartek <bartiqpuchala@gmail.com>
2023-01-07 07:15:40 +0100

    Abstract factory design pattern added

--- src/main/java/com/example/checkers/CheckersMenu.java
+++ src/main/java/com/example/checkers/CheckersMenu.java
@@ -9,15 +9,14 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class CheckersMenu extends Application implements Runnable {
+public class CheckersMenu extends Application {
 
-  Stage newStage;
-
-  public CheckersMenu() {
-
-  }
+  private static Stage newStage;
+  private CheckersBoard board;
+  private CheckersApp app;
 
   @Override
   public void start(Stage stage) throws Exception {
@@ -30,55 +29,58 @@ public class CheckersMenu extends Application implements Runnable {
 
     Button game1 = new Button("Warcaby klasyczne");
     Button game2 = new Button("Warcaby polskie");
-    Button game3 = new Button("Warcaby tureckie");
+    Button game3 = new Button("Warcaby rosyjskie");
 
-    EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
-
+    EventHandler<MouseEvent> classicHandler = new EventHandler<MouseEvent>() {
       @Override
       public void handle(MouseEvent mouseEvent) {
-        CheckersApp app = new CheckersApp();
+        app = new CheckersApp();
         Thread th = new Thread(()-> {
           try {
-            int SIZE = 10;
-            app.initApp(SIZE, newStage);
+            board = new ClassicBoard();
+            app.initApp(board, newStage);
           } catch(Exception e) {
             System.out.println(e);
           }
         });
         th.start();
-//        CheckersApp app2 = new CheckersApp();
-//        Thread th2 = new Thread(()-> {
-//          try {
-//            app2.start(stage2);
-//          } catch(Exception e) {
-//            System.out.println(e);
-//          }
-//        });
-//        th2.start();
-
       }
     };
 
-    EventHandler<MouseEvent> eventHandler2 = new EventHandler<MouseEvent>() {
+    EventHandler<MouseEvent> polishHandler = new EventHandler<MouseEvent>() {
       @Override
       public void handle(MouseEvent mouseEvent) {
-        Stage stage2 = new Stage();
-
-          CheckersApp app = new CheckersApp();
-
-          Thread th = new Thread(()-> {
-            try {
-              app.start(stage2);
-            } catch(Exception e) {
-              System.out.println(e);
-            }
-          });
-          th.start();
+        app = new CheckersApp();
+        Thread th = new Thread(()-> {
+          try {
+            board = new PolishBoard();
+            app.initApp(board, newStage);
+          } catch(Exception e) {
+            System.out.println(e);
+          }
+        });
+        th.start();
+      }
+    };
+    EventHandler<MouseEvent> russianHandler = new EventHandler<MouseEvent>() {
+      @Override
+      public void handle(MouseEvent mouseEvent) {
+        app = new CheckersApp();
+        Thread th = new Thread(()-> {
+          try {
+            board = new RussianBoard();
+            app.initApp(board, newStage);
+          } catch(Exception e) {
+            System.out.println(e);
+          }
+        });
+        th.start();
       }
     };
 
-    game1.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
-    game2.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler2);
+    game1.addEventFilter(MouseEvent.MOUSE_CLICKED, classicHandler);
+    game2.addEventFilter(MouseEvent.MOUSE_CLICKED, polishHandler);
+    game3.addEventFilter(MouseEvent.MOUSE_CLICKED, russianHandler);
 
     root.getChildren().addAll(game1, game2, game3);
     root.setAlignment(Pos.CENTER);
@@ -92,10 +94,5 @@ public class CheckersMenu extends Application implements Runnable {
     newStage.setTitle("Checkers Menu");
     newStage.setScene(scene);
     newStage.show();
-  }
-
-  @Override
-  public void run() {
-
   }
 }
