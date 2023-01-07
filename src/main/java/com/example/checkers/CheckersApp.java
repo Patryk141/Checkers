fa90197cc35476932b6f9bb7c448a08fd8175a22
Bartek <bartiqpuchala@gmail.com>
2023-01-07 07:16:17 +0100

    Added alert notification after client wins the game

--- src/main/java/com/example/checkers/CheckersApp.java
+++ src/main/java/com/example/checkers/CheckersApp.java
@@ -12,16 +12,15 @@ import javafx.stage.Stage;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import static java.lang.Integer.parseInt;
 
 public class CheckersApp extends Application implements Runnable, EventHandler<MouseEvent> {
 
-    public static final int PieceSize = 80;
-    public static int Size;
-    int i, j;
+    public static int PieceSize;
     Piece piece;
-    public static Square[][] squares;
+    public Square[][] squares;
 
     Socket socket = null;
     PrintWriter out = null;
@@ -43,47 +42,8 @@ public class CheckersApp extends Application implements Runnable, EventHandler<M
 
     }
 
-    private Pane CreateBoard() {
-        Pane root = new Pane();
-        squares = new Square[Size][Size];
-        Group squareGroup = new Group();
-        Group pieceGroup = new Group();
-        root.setPrefSize(Size*PieceSize,Size*PieceSize);
-        for(i=0 ; i<Size ; i++) {
-            for(j=0 ; j<Size ; j++){
-                squares[i][j] = new Square(i,j,(i+j)%2);
-                if((i+j)%2 != 0) {
-                    if(Size-4 < j){
-                        piece = new Piece(i,j,PieceType.BLACK);
-                        squares[i][j].setPiece(piece); // ustawienie na kwadratach pionków
-                        if(player == PLAYER2) {
-                            piece.setOnMouseDragged(this);
-                            piece.setOnMouseReleased(this);
-                        }
-                        pieceGroup.getChildren().add(piece);
-                    }
-                    else if(3 > j){
-                        piece = new Piece(i,j,PieceType.WHITE);
-                        squares[i][j].setPiece(piece);
-                        if(player == PLAYER1) {
-                            piece.setOnMouseDragged(this);
-                            piece.setOnMouseReleased(this);
-                        }
-                        pieceGroup.getChildren().add(piece);
-                    }
-
-                }
-                squareGroup.getChildren().add(squares[i][j]);
-            }
-        }
-
-        root.getChildren().addAll(squareGroup,pieceGroup);
-        return root;
-    }
-
     @Override
     public void handle(MouseEvent e) {
-
         Piece currPiece = (Piece)e.getSource();
         if (e.getEventType() == MouseEvent.MOUSE_DRAGGED) {
             double dx = e.getX() - currPiece.getCenterX();
@@ -196,20 +156,18 @@ public class CheckersApp extends Application implements Runnable, EventHandler<M
     private void changePosition(String move) {
         String[] date_xy = move.split(" ");
         Piece mypiece = squares[parseInt(date_xy[1])][parseInt(date_xy[2])].getPiece();
-        mypiece.setCenterX(parseInt(date_xy[4])*CheckersApp.PieceSize+CheckersApp.PieceSize*0.5);
-        mypiece.setCenterY(parseInt(date_xy[5])*CheckersApp.PieceSize+CheckersApp.PieceSize*0.5);
+        mypiece.setCenterX(parseInt(date_xy[4])*PieceSize+PieceSize*0.5);
+        mypiece.setCenterY(parseInt(date_xy[5])*PieceSize+PieceSize*0.5);
         mypiece.setX_pos(parseInt(date_xy[4])); // setting new x_pos
         mypiece.setY_pos(parseInt(date_xy[5])); // setting new y_pos
-        mypiece.setOldX(mypiece.getCenterX() - mypiece.getCenterX() % CheckersApp.PieceSize + CheckersApp.PieceSize * 0.5);
-        mypiece.setOldY(mypiece.getCenterY() - mypiece.getCenterY() % CheckersApp.PieceSize + CheckersApp.PieceSize * 0.5);
+        mypiece.setOldX(mypiece.getCenterX() - mypiece.getCenterX() % PieceSize + PieceSize * 0.5);
+        mypiece.setOldY(mypiece.getCenterY() - mypiece.getCenterY() % PieceSize + PieceSize * 0.5);
         squares[parseInt(date_xy[1])][parseInt(date_xy[2])].setPiece(null); // setting old piece to null
         squares[mypiece.getX_pos()][mypiece.getY_pos()].setPiece(mypiece); // setting new piece
     }
     private void removePiece(String move) {
         String[] date_xy = move.split(" ");
-//        Piece removedPiece = squares[(parseInt(date_xy[1]) + parseInt(date_xy[4])) / 2][(parseInt(date_xy[2]) + parseInt(date_xy[5])) / 2].getPiece();
-//        squares[(parseInt(date_xy[1]) + parseInt(date_xy[4])) / 2][(parseInt(date_xy[2]) + parseInt(date_xy[5])) / 2].setPiece(null);
-//        removedPiece.setVisible(false);
+
         int oldX = parseInt(date_xy[1]);
         int oldY = parseInt(date_xy[2]);
         int newX = parseInt(date_xy[4]);
@@ -262,7 +220,6 @@ public class CheckersApp extends Application implements Runnable, EventHandler<M
     public void receiveInfoFromServer() {
         try {
             player = parseInt(in.readLine()); // ustawienie
-            System.out.println("ELOOO");
             System.out.println("Witam z tej strony Player " + player);
             if(player == 1){
                 yourTurn = true;
@@ -278,50 +235,48 @@ public class CheckersApp extends Application implements Runnable, EventHandler<M
         gTh.start();
     }
 
-    private void initBoard(int size) {
-        Size = size;
-        Game.size = size;
-        try {
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-            writer.println("" + size);
-        } catch(IOException err) {
-            System.out.println(err);
+    private void initHandlers(ArrayList<Piece> blackPieces, ArrayList<Piece> whitePieces) {
+
+        for(Piece blackPiece: blackPieces) {
+            if(player == PLAYER2) {
+                blackPiece.setOnMouseDragged(this);
+                blackPiece.setOnMouseReleased(this);
+            }
         }
-        System.out.println(Game.size);
+
+        for(Piece whitePiece: whitePieces) {
+            if(player == PLAYER1) {
+                whitePiece.setOnMouseDragged(this);
+                whitePiece.setOnMouseReleased(this);
+            }
+        }
     }
 
-    public void initApp(int SIZE, Stage stage) {
+    public void initApp(CheckersBoard board, Stage stage) {
         listenSocket();
         receiveInfoFromServer();
-        initBoard(SIZE);
+        board.sendMsgToServer(socket);
+        // Creating the ui(board, pieces, squares, enabling the pieces to move)
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
                 stage.close();
-                Stage stage = new Stage();
-                Scene scene = new Scene(CreateBoard());
-                stage.setTitle("Player " + player);
-                stage.setScene(scene);
-                stage.show();
+                Stage newStage = new Stage();
+                PieceSize = board.getPieceSize();
+                Scene scene = new Scene(board.createBoard());
+                squares = board.getSquares();
+                initHandlers(board.getBlackPieces(), board.getWhitePieces()); // activating handlers
+                newStage.setTitle("Player " + player);
+                newStage.setScene(scene);
+                newStage.show();
             }
         });
         startThread();
     }
 
-
     @Override
     public void start(Stage primaryStage) throws Exception { // wywołana wtedy, kiedy aplikacja JavaFX startuje
-//        CheckersMenu checkersMenu = new CheckersMenu();
-//        checkersMenu.initMenu();
-//        listenSocket();
-//        receiveInfoFromServer();
-//        Platform.runLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                initApp();
-//            }
-//        });
-//        startThread();
+
     }
 
     public static void main(String[] args) {
