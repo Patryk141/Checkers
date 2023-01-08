22527f41f193aee703750aa4a1a029a4c004db9f
Bartek <bartiqpuchala@gmail.com>
2023-01-08 08:00:08 +0100

    Interface with the methods that server is calling

--- src/main/java/com/example/checkers/GameRules.java
+++ src/main/java/com/example/checkers/GameRules.java
@@ -1,11 +1,21 @@
 package com.example.checkers;
 
-public class GameRules {
+public interface GameRules {
 
-  private Square[] squares;
-
-  public GameRules(Square[] squares) {
-    this.squares = squares;
-  }
+  String availableMoves(Piece piece);
+  void availablePiece(int turn, int size, Square[][] squares);
+  boolean checkIfMatted(int oldX, int oldY, int newX, int newY);
+  boolean checkMove(int oldX, int oldY, int newX, int newY);
+  void promotion(int x, int y);
+  boolean checkBlockedPieces(PieceType type);
+  void setNumberOfPieces(int blackPieces, int whitePieces);
+  String getAvailablePiece();
+  String getObligatoryPiece();
+  int getBlackPieces();
+  int getWhitePieces();
+  void setAvailablePiece(String availablePiece);
+  void setObligatoryPiece(String obligatoryPiece);
+  void setBlackPieces(int blackPieces);
+  void setWhitePieces(int whitePieces);
 
 }
